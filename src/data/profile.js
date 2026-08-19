28fd44771f7b157c4d490c6414e84cfb6e4069f4
nabihossen9911 <nabihossen9911@gmail.com>
2026-08-19 22:52:37 +0600

    Improve vCard contact

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,29 +1,29 @@
 const profile = {
+  // Contact Information
   name: "Nabi Hossen",
-
   firstName: "Nabi",
   lastName: "Hossen",
 
   position: "Founder & CEO",
-
   company: "NITS Computer Training Institute",
+
   tagline: "Start IT - Grow Your Skills - Be Confident in Your Career",
 
+  // Contact
   phone: "+8801567818036",
   whatsapp: "8801567818036",
-
   email: "info@nitscti.com",
 
+  // Website & Social
   website: "https://nitscti.com",
-
   facebook: "https://facebook.com/nitscti",
-
   youtube: "https://www.youtube.com/@nitscti",
 
+  // Address
   address: "Borabo Bazar, Jatramura, Rupganj, Narayanganj",
-
   maps: "https://maps.google.com/?q=PGH9+9GR+Borabo",
 
+  // Popular Courses
   courses: [
     "Office Course",
     "IT Course",
@@ -31,7 +31,7 @@ const profile = {
     "Digital Marketing",
     "Video Editing",
     "Web Development",
-  ]
+  ],
 };
 
 export default profile;
--- src/utils/vcard.js
+++ src/utils/vcard.js
@@ -1,3 +1,4 @@
+import logoBase64 from "../data/logoBase64";
 import profile from "../data/profile";
 
 export function downloadVCard() {
@@ -7,12 +8,13 @@ VERSION:3.0
 N:${profile.lastName};${profile.firstName};;;
 FN:${profile.name}
 ORG:${profile.company}
-NOTE:${profile.tagline}
 TITLE:${profile.position}
-TEL;TYPE=CELL:${profile.phone}
+NOTE:${profile.tagline}
+TEL;TYPE=CELL,VOICE:${profile.phone}
 EMAIL;TYPE=INTERNET:${profile.email}
 URL:${profile.website}
 ADR;TYPE=WORK:;;${profile.address};;;;Bangladesh
+PHOTO;ENCODING=b;TYPE=PNG:${logoBase64}
 END:VCARD
 `.trim();
 
@@ -25,6 +27,7 @@ END:VCARD
   const link = document.createElement("a");
   link.href = url;
   link.download = `${profile.name}.vcf`;
+
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
